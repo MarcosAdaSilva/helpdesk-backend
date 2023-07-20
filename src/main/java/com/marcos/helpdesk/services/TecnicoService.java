fb09fb3bd9585016423f18709ddccc29bc6f8e99
MarcosAdaSilva <marcaofly@gmail.com>
2023-07-19 23:45:37 -0300

    Tratando exceções para objetos não encontrados

--- src/main/java/com/marcos/helpdesk/services/TecnicoService.java
+++ src/main/java/com/marcos/helpdesk/services/TecnicoService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.marcos.helpdesk.domain.Tecnico;
 import com.marcos.helpdesk.repositories.TecnicoRepository;
+import com.marcos.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
-	
+
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
 
 }
