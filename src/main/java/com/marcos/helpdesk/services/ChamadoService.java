c9aab0942f969f69ee3a865a073688e23bb33443
MarcosAdaSilva <marcaofly@gmail.com>
2023-08-23 09:07:44 -0300

    update chamado implementado

--- src/main/java/com/marcos/helpdesk/services/ChamadoService.java
+++ src/main/java/com/marcos/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package com.marcos.helpdesk.services;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -40,6 +41,13 @@ public class ChamadoService {
 	public Chamado create(@Valid ChamadoDTO objDTO) {
 		return repository.save(newChamado(objDTO));
 	}
+	
+	public Chamado update(Integer id, @Valid ChamadoDTO objDTO) {
+		objDTO.setId(id);
+		Chamado oldObj = findById(id);
+		oldObj = newChamado(objDTO);
+		return repository.save(oldObj);
+	}
 
 	private Chamado newChamado(ChamadoDTO obj) {
 		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
@@ -48,6 +56,10 @@ public class ChamadoService {
 		Chamado chamado = new Chamado();
 		if (obj.getId() != null) {
 			chamado.setId(obj.getId());
+		}
+		
+		if(obj.getStatus().equals(2)) {
+			chamado.setDataFechamento(LocalDate.now());
 		}
 
 		chamado.setTecnico(tecnico);
@@ -59,4 +71,5 @@ public class ChamadoService {
 		return chamado;
 
 	}
+
 }
